9db7dbd739e1faa86347bd7f0564ea177693de00
jjeisy <48239691+jjeisy@users.noreply.github.com>
2020-03-01 12:06:08 +0100

    Entwurf App

---
+++ Software/Android/app/src/main/java/ch/fhnw/pro4e_team1_sphere/MainActivity.java
@@ -0,0 +1,14 @@
+package ch.fhnw.pro4e_team1_sphere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
